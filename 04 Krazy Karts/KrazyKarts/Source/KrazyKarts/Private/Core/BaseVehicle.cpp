b01bb10ceed52dfefda445c378c0955028060a8b
Vadim <3dwad@mail.ru>
2020-04-05 15:53:37 +0300

    Ready 076 Steering And Turning Circles

--- 04 Krazy Karts/KrazyKarts/Source/KrazyKarts/Private/Core/BaseVehicle.cpp
+++ 04 Krazy Karts/KrazyKarts/Source/KrazyKarts/Private/Core/BaseVehicle.cpp
@@ -3,23 +3,27 @@
 
 #include "Core/BaseVehicle.h"
 
+#include "Components/BoxComponent.h"
 #include "Components/InputComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
-
 #include "Engine/Engine.h"
+#include "Engine/World.h"
 
 
 
 // Sets default values
 ABaseVehicle::ABaseVehicle()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
+	CollisionBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBoxComponent->SetupAttachment(RootComponent);
+
 	SkelMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
-	SkelMeshComponent->SetupAttachment(RootComponent);
+	SkelMeshComponent->SetupAttachment(CollisionBoxComponent);
 
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArmComponent->SetupAttachment(SkelMeshComponent);
@@ -27,50 +31,105 @@ ABaseVehicle::ABaseVehicle()
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComponent->SetupAttachment(SpringArmComponent);
 
-	
 }
 
 // Called when the game starts or when spawned
 void ABaseVehicle::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
 void ABaseVehicle::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);	
+
+	Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+
+	Force += GetAirResistance();
+	Force += GetRollingResistance();
+
+	FVector Acceleration = Force / Mass;
+
+	Velocity = Velocity + Acceleration * DeltaTime;
+	
+	ApplyRotation(DeltaTime);
+
+	/* dX = V * dT */
+	UpdateLocationFromVelocity(DeltaTime);	
+}
+
+FVector ABaseVehicle::GetAirResistance()
+{
+	float Speed = Velocity.SizeSquared();
+
+	GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Green, "Velocity value is:" + FString::SanitizeFloat(Speed));
+
+	return Speed * DragCoefficient * - Velocity.GetSafeNormal();	
+}
+
+FVector ABaseVehicle::GetRollingResistance()
+{
+	float GravityStrenght = -GetWorld()->GetGravityZ() / 100;
+
+	float NormalForce = Mass * GravityStrenght;
+
+	return -Velocity.GetSafeNormal() *RolingResistanceCoefficient * NormalForce;
+}
+
+void ABaseVehicle::UpdateLocationFromVelocity(float DeltaTime)
+{
+
+	FVector DeltaLocation = Velocity * 100 * DeltaTime;
+	FHitResult Result;
+	AddActorWorldOffset(DeltaLocation, true, &Result);
+
+	if (Result.IsValidBlockingHit())
+	{
+		Velocity = FVector::ZeroVector;
+	}
+
+}
+
+void ABaseVehicle::ApplyRotation(float DeltaTime)
+{
+	float DeltaLocation = FVector::DotProduct(GetActorForwardVector(),Velocity) * DeltaTime;
+
+	float RotationAngle = DeltaLocation / MinimumTurningRadius * SteeringThrow;
+
+	FQuat RotationDelta(GetActorUpVector(), RotationAngle);
 
 
-	 FVector DeltaLocation = Velocity * DeltaTime;
+	Velocity = RotationDelta.RotateVector(Velocity);
 
-	 AddActorWorldOffset(DeltaLocation, true);
-
-
-
-	//GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Green, FString::SanitizeFloat(ForwarAxisValue));
-
+	AddActorWorldRotation(RotationDelta);
 }
 
 // Called to bind functionality to input
 void ABaseVehicle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	
-	PlayerInputComponent->BindAxis("MoveForward", this, &ABaseVehicle::MoveForward);
 
-	
-	
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABaseVehicle::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABaseVehicle::MoveRight);
+
+
+
 }
 
 void ABaseVehicle::MoveForward(float InValue)
 {
-	
+
 	GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Green, "Forward value is:" + FString::SanitizeFloat(InValue));
 
+	Throttle = InValue;
+}
 
-	Velocity = GetActorForwardVector()* Speed * InValue;
+void ABaseVehicle::MoveRight(float InValue)
+{
+
+	SteeringThrow = InValue;
 
 }
 
--- 04 Krazy Karts/KrazyKarts/Source/KrazyKarts/Public/Core/BaseVehicle.h
+++ 04 Krazy Karts/KrazyKarts/Source/KrazyKarts/Public/Core/BaseVehicle.h
@@ -10,6 +10,7 @@
 class USkeletalMeshComponent;
 class USpringArmComponent;
 class UCameraComponent;
+class UBoxComponent;
 
 UCLASS()
 class KRAZYKARTS_API ABaseVehicle : public APawn
@@ -29,6 +30,9 @@ public:
 	UPROPERTY(VisibleAnywhere, Category = "CameraSettings")
 	UCameraComponent* CameraComponent;
 
+	UPROPERTY(VisibleAnywhere, Category = "VehicleSettings")
+	UBoxComponent* CollisionBoxComponent;
+
 	
 
 	float ForwarAxisValue;
@@ -43,17 +47,56 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(BlueprintReadOnly)
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector Force;
+
+	UFUNCTION(BlueprintCallable ,BlueprintPure)
+	FVector GetAirResistance();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetRollingResistance();
+	
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	void MoveForward(float InValue);
 
+	void MoveRight(float InValue);
+
 
 private:
 
 	
-	FVector Velocity;
+
+	/* In kg*/
+	UPROPERTY(EditDefaultsOnly, Category = "MovementSettings")
+	float Mass = 1000.f;
+
+	/* The force applied to the car when the throttle is fully down (Newton)*/
+	UPROPERTY(EditDefaultsOnly, Category = "MovementSettings")
+	float MaxDrivingForce = 10000.f;
+			
+	/* The Drag with air. Higher value mean more drag. kg/meter*/
+	UPROPERTY(EditDefaultsOnly, Category = "MovementSettings")
+	float DragCoefficient = 16.f;
+
+	/* kg/meter*/
+	UPROPERTY(EditDefaultsOnly, Category = "MovementSettings")
+	float RolingResistanceCoefficient = 0.03f;
 
 	UPROPERTY(EditDefaultsOnly, Category = "MovementSettings")
-	float Speed = 1666.667;
+	float MinimumTurningRadius = 10.f;
+
+
+	float Throttle;
+	float SteeringThrow;
+
+
+
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+	void ApplyRotation(float DeltaTime);
 };
